15dc4b449558f68999d345424b6db32e2f0a6c54
DmitryNickov <82496076+DmitryNickov@users.noreply.github.com>
2021-04-20 16:07:51 +0500

    Update init.sh
    add strings formatting
    comment old gunicorn parts

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 sudo apt-get update
 sudo apt-get install -y python3.5
+
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
+
 virtualenv -p python3.5 /home/box/web/venv/
 source /home/box/web/venv/bin/activate
+
 pip install django==2.1
 pip install gunicorn
-sudo gunicorn -b 0.0.0.0:8080 --chdir /home/box/web/etc hello:app &
+
+#sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo gunicorn -b 0.0.0.0:8080 --chdir /home/box/web/etc hello:app &
