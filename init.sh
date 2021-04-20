0065b86e358360b185d156709092af9f4e685d04
DmitryNickov <82496076+DmitryNickov@users.noreply.github.com>
2021-04-20 13:12:04 +0500

    Update init.sh
    add setting virtualenv and python, django, gunicorn updating

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
+sudo apt-get update
+sudo apt-get install -y python3.5
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
+virtenv -p python3.5 /home/box/web/venv/
+source /home/box/web/venv/bin/activate
+pip install django==2.1
+pip install gunicorn
 sudo gunicorn -b 0.0.0.0:8080 --chdir /home/box/web/etc hello:app &
