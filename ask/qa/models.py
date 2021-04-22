240cf752e638c9ec3761a51782825507f6685654
DmitryNickov <pgs.gorod@gmail.com>
2021-04-22 17:52:21 +0500

    fix models.py QuestionManager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=150)
@@ -12,13 +19,6 @@ class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
-
-class QestionManager(models.Model):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
 
 
 class Answer(models.Model):
