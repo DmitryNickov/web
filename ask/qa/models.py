493581fed1861af3daed12e7835a1730208e60a0
DmitryNickov <pgs.gorod@gmail.com>
2021-04-22 17:24:21 +0500

    change models.py - add QuestionManager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=150)
     text = models.TextField(blank=True)
     added_at = models.DateField(auto_now_add=True)
@@ -11,6 +12,8 @@ class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='question_like_user')
 
+
+class QestionManager(models.Model):
     def new(self):
         return self.order_by('-added_at')
 
